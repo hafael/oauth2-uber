7a7455c2d54740da09351e132a2215a1a4fc3ee3
Rafael Villa-Verde <villa655321verde@gmail.com>
2019-04-05 21:03:40 -0300

    add files

---
+++ CHANGELOG.md
@@ -0,0 +1,75 @@
+# Changelog
+All Notable changes to `oauth2-uber` will be documented in this file
+
+## 2.0.0 - 2017-01-25
+
+### Added
+- PHP 7.1 Support
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- PHP 5.5 Support
+
+### Security
+- Nothing
+
+## 1.0.0 - 2017-01-25
+
+Bump for base package parity
+
+## 0.2.1 - 2016-10-17
+
+### Added
+- Nothing
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Scope separator was incorrectly set to comma when space is required; it is now a space.
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
+
+
+## 0.2.0 - 2015-08-20
+
+### Added
+- Upgrade to support version 1.0 release of core client
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
+
+## 0.1.0 - 2015-03-21
+
+### Added
+- Initial release!
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
---
+++ test/src/Provider/UberTest.php
@@ -0,0 +1,150 @@
+<?php namespace Stevenmaguire\OAuth2\Client\Test\Provider;
+
+use League\OAuth2\Client\Tool\QueryBuilderTrait;
+use Mockery as m;
+
+class UberTest extends \PHPUnit_Framework_TestCase
+{
+    use QueryBuilderTrait;
+
+    protected $provider;
+
+    protected function setUp()
+    {
+        $this->provider = new \Stevenmaguire\OAuth2\Client\Provider\Uber([
+            'clientId' => 'mock_client_id',
+            'clientSecret' => 'mock_secret',
+            'redirectUri' => 'none',
+        ]);
+    }
+
+    public function tearDown()
+    {
+        m::close();
+        parent::tearDown();
+    }
+
+    public function testAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+        parse_str($uri['query'], $query);
+
+        $this->assertArrayHasKey('client_id', $query);
+        $this->assertArrayHasKey('redirect_uri', $query);
+        $this->assertArrayHasKey('state', $query);
+        $this->assertArrayHasKey('scope', $query);
+        $this->assertArrayHasKey('response_type', $query);
+        $this->assertArrayHasKey('approval_prompt', $query);
+        $this->assertNotNull($this->provider->getState());
+    }
+
+
+    public function testScopes()
+    {
+        $scopeSeparator = ' ';
+        $options = ['scope' => [uniqid(), uniqid()]];
+        $query = ['scope' => implode($scopeSeparator, $options['scope'])];
+        $url = $this->provider->getAuthorizationUrl($options);
+        $encodedScope = $this->buildQueryString($query);
+        $this->assertContains($encodedScope, $url);
+    }
+
+    public function testGetAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+
+        $this->assertEquals('/oauth/v2/authorize', $uri['path']);
+    }
+
+    public function testGetBaseAccessTokenUrl()
+    {
+        $params = [];
+
+        $url = $this->provider->getBaseAccessTokenUrl($params);
+        $uri = parse_url($url);
+
+        $this->assertEquals('/oauth/v2/token', $uri['path']);
+    }
+
+    public function testGetAccessToken()
+    {
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody')->andReturn('{"access_token": "mock_access_token","token_type": "Bearer","expires_in": 3600,"refresh_token": "mock_refresh_token","scope": "profile history"}');
+        $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $response->shouldReceive('getStatusCode')->andReturn(200);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(1)->andReturn($response);
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $this->assertEquals('mock_access_token', $token->getToken());
+        $this->assertLessThanOrEqual(time() + 3600, $token->getExpires());
+        $this->assertGreaterThanOrEqual(time(), $token->getExpires());
+        $this->assertEquals('mock_refresh_token', $token->getRefreshToken());
+        $this->assertNull($token->getResourceOwnerId());
+    }
+
+    public function testUserData()
+    {
+        $email = uniqid();
+        $userId = rand(1000,9999);
+        $firstName = uniqid();
+        $lastName = uniqid();
+        $picture = uniqid();
+        $coupon = uniqid();
+
+        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $postResponse->shouldReceive('getBody')->andReturn('{"access_token": "mock_access_token","token_type": "Bearer","expires_in": 3600,"refresh_token": "mock_refresh_token","scope": "profile history"}');
+        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $postResponse->shouldReceive('getStatusCode')->andReturn(200);
+
+        $userResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $userResponse->shouldReceive('getBody')->andReturn('{"first_name": "'.$firstName.'","last_name": "'.$lastName.'","email": "'.$email.'","picture": "'.$picture.'","promo_code": "'.$coupon.'","uuid": "'.$userId.'"}');
+        $userResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $userResponse->shouldReceive('getStatusCode')->andReturn(200);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')
+            ->times(2)
+            ->andReturn($postResponse, $userResponse);
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+        $user = $this->provider->getResourceOwner($token);
+
+        $this->assertEquals($email, $user->getEmail());
+        $this->assertEquals($email, $user->toArray()['email']);
+        $this->assertEquals($userId, $user->getId());
+        $this->assertEquals($userId, $user->toArray()['uuid']);
+        $this->assertEquals($firstName, $user->getFirstname());
+        $this->assertEquals($firstName, $user->toArray()['first_name']);
+        $this->assertEquals($lastName, $user->getLastname());
+        $this->assertEquals($lastName, $user->toArray()['last_name']);
+        $this->assertEquals($picture, $user->getImageurl());
+        $this->assertEquals($picture, $user->toArray()['picture']);
+    }
+
+    /**
+     * @expectedException League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     **/
+    public function testExceptionThrownWhenErrorObjectReceived()
+    {
+        $message = uniqid();
+        $status = rand(400,600);
+        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $postResponse->shouldReceive('getBody')->andReturn('{"message": "'.$message.'","code": "invalid","fields": {"first_name": ["Required"]}}');
+        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $postResponse->shouldReceive('getStatusCode')->andReturn($status);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')
+            ->times(1)
+            ->andReturn($postResponse);
+        $this->provider->setHttpClient($client);
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+    }
+}
